53dc1da84467680f014b130dab6bb0df143eb272
gyk4j <147011991+gyk4j@users.noreply.github.com>
2024-08-26 13:04:45 +0800

    Add file (SMB) container

--- v-ubuntu22.sh
+++ v-ubuntu22.sh
@@ -15,6 +15,9 @@ echo \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
 
+# Install networking tools
+apt-get install -y net-tools smbclient
+
 # Install Docker
 apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
   
