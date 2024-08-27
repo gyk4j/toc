138b7c7fae3e2a70f27c8da1c13c2be9898a0ffa
gyk <147011991+gyk4j@users.noreply.github.com>
2024-08-27 14:23:34 +0800

    Generate keystore on startup (#11)

--- mail/startup.sh
+++ mail/startup.sh
@@ -4,6 +4,10 @@ if [[ ! -e /root/conf/private.key ]]; then
     openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=US/ST=Apache/L=Fundation/O=/CN=james.apache.org" -keyout /root/conf/private.key -out /root/conf/private.csr
 fi
 
+if [[ ! -e /root/conf/keystore ]]; then
+    keytool -genkeypair -alias james -keyalg RSA -storetype PKCS12 -keystore /root/conf/keystore -storepass password -dname "C=US,ST=Delaware,L=Wilmington,O=Apache Software Foundation,CN=James Apache"
+fi
+
 wait-for-it.sh --host=localhost --port=9999 --strict --timeout=0 -- ./initialdata.sh &
 
 java -Djdk.tls.ephemeralDHKeySize=2048 \
