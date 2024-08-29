38d9ecff5597c6886df6228b2d428d6e5c483338
gyk <147011991+gyk4j@users.noreply.github.com>
2024-08-29 14:02:31 +0800

    Move docker exec test commands into Go app (#19)

--- toc/toc.go
+++ toc/toc.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "log"
+  "os/exec"
+  "strings"
+  "time"
+)
 
 func main() {
-    fmt.Println("hello world")
+  Exec("toc-db-1")
+  Exec("toc-file-1")
+  Exec("toc-mail-1")
+  Exec("toc-web-1")
+  time.Sleep(2 * time.Minute)
+}
+
+func Exec(container string) {
+  cmd := exec.Command("docker", "exec", container, "cat", "/etc/hostname")
+  var out strings.Builder
+	cmd.Stdout = &out
+  err := cmd.Run()
+  if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s: %s\n", container, out.String())
 }
