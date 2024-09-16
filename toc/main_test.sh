111c53b68df80d38bb91cd9b3dedacfe09ecf15d
gyk <147011991+gyk4j@users.noreply.github.com>
2024-09-16 12:20:58 +0800

    Add archive API implementation (#33)

--- toc/main_test.sh
+++ toc/main_test.sh
@@ -1,2 +1,15 @@
 #!/bin/sh
 
+echo --- Test hello world
+curl -sSL -X GET  http://localhost:8080/v1/
+
+echo --- Test new archive
+curl -sSL -X POST http://localhost:8080/v1/archives | json_pp
+
+echo --- Test get all archives
+curl -sSL -X GET  http://localhost:8080/v1/archives | json_pp
+
+echo --- Test get archive by ID
+curl -sSL -X GET  http://localhost:8080/v1/archives/0 | json_pp
+
+
