4bf5ca0fbfd4a5c995314fa62c6e360e6b5e5c9e
gyk4j <147011991+gyk4j@users.noreply.github.com>
2024-09-19 09:07:33 +0800

    Update API test script

--- toc/server/main_test.sh
+++ toc/server/main_test.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-echo --- Test hello world
-curl -sSL -X GET  http://localhost:8080/v1/
-
 echo --- Test new archive
 curl -sSL -X POST http://localhost:8080/v1/archives | json_pp
 
@@ -12,4 +9,35 @@ curl -sSL -X GET  http://localhost:8080/v1/archives | json_pp
 echo --- Test get archive by ID
 curl -sSL -X GET  http://localhost:8080/v1/archives/0 | json_pp
 
+echo --- Test new backup
+curl -sSL -X POST http://localhost:8080/v1/backups | json_pp
 
+echo --- Test get all backups
+curl -sSL -X GET  http://localhost:8080/v1/backups | json_pp
+
+echo --- Test get backup by ID
+curl -sSL -X GET  http://localhost:8080/v1/backups/0 | json_pp
+
+echo --- Test get backup by invalid ID
+curl -sSL -X GET  http://localhost:8080/v1/backups/999 | json_pp
+
+echo --- Test new restoration
+curl -sSL -X POST http://localhost:8080/v1/restorations | json_pp
+
+echo --- Test get all restorations
+curl -sSL -X GET  http://localhost:8080/v1/restorations | json_pp
+
+echo --- Test get restoration by ID
+curl -sSL -X GET  http://localhost:8080/v1/restorations/0 | json_pp
+
+echo --- Test get restoration by invalid ID
+curl -sSL -X GET  http://localhost:8080/v1/restorations/9999 | json_pp
+
+echo --- Test new log export request
+curl -sSL -X POST http://localhost:8080/v1/logs | json_pp
+
+echo --- Test get all file/disk quotas
+curl -sSL -X GET  http://localhost:8080/v1/quotas | json_pp
+
+echo --- Test new synchronization request
+curl -sSL -X POST http://localhost:8080/v1/synchronizations | json_pp
