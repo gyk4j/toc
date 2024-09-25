e7e4a656927d82296a9606d882988efd48c89380
gyk4j <147011991+gyk4j@users.noreply.github.com>
2024-09-25 15:44:00 +0800

    Add s6-overlay to Apache James

--- mail/startup.sh
+++ mail/startup.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-if [[ ! -e /root/conf/private.key ]]; then
-    openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=US/ST=Apache/L=Fundation/O=/CN=james.apache.org" -keyout /root/conf/private.key -out /root/conf/private.csr
+# If user has not COPY or mapped one from VOLUME, generate a self-signed private key.
+if [ ! -f "/root/conf/private.key" ]; then
+    openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 \
+      -subj "/C=US/ST=Delaware/L=Wilmington/O=Apache Software Foundation/CN=Apache James" \
+      -keyout /root/conf/private.key \
+      -out /root/conf/private.csr
 fi
 
-if [[ ! -e /root/conf/keystore ]]; then
-    keytool -genkeypair -alias james -keyalg RSA -storetype PKCS12 -keystore /root/conf/keystore -storepass password -dname "C=US,ST=Delaware,L=Wilmington,O=Apache Software Foundation,CN=James Apache"
+# If user has not COPY or mapped one from VOLUME, generate a test keystore and keypair.
+if [ ! -f "/root/conf/keystore" ]; then
+    keytool -genkeypair -alias james -keyalg RSA -storetype PKCS12 \
+      -keystore /root/conf/keystore -storepass password \
+      -dname "C=US,ST=Delaware,L=Wilmington,O=Apache Software Foundation,CN=Apache James"
 fi
 
-wait-for-it.sh --host=localhost --port=9999 --strict --timeout=0 -- ./initialdata.sh &
+wait-for-it.sh --host=localhost --port=9999 --strict --timeout=0 -- /root/initialdata.sh &
 
 java -Djdk.tls.ephemeralDHKeySize=2048 \
      -classpath '/root/resources:/root/classes:/root/libs/*' \
