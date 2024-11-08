b82b3ecf825f5cc5c3c118d2d441aae7b16d3b59
gyk4j <147011991+gyk4j@users.noreply.github.com>
2024-11-08 14:55:09 +0800

    Update controller to return precise error code/status

--- toc-backend/services/controller/controller.go
+++ toc-backend/services/controller/controller.go
@@ -21,6 +21,8 @@ type BackupService struct {
 }
 
 func (s *BackupService) NewBackup() (*models.Backup, *services.AppError) {
+	var err *services.AppError
+
 	b := models.Backup{
 		ID:        -1,
 		Time:      strfmt.DateTime(time.Now()),
@@ -39,27 +41,74 @@ func (s *BackupService) NewBackup() (*models.Backup, *services.AppError) {
 		b.Snapshots = append(b.Snapshots, &ss)
 	}
 
-	return s.r.Save(&b), nil
+	o := s.r.Save(&b)
+
+	if o == nil {
+		err = &services.AppError{
+			Error:   fmt.Errorf("internal server error: %s", "NewBackup"),
+			Message: "internal server error",
+			Code:    int(services.NewBackupInternalServerError),
+		}
+	}
+
+	return o, err
 }
 
 func (s *BackupService) UpdateBackup(backup *models.Backup) (*models.Backup, *services.AppError) {
-	if s.r.FindById(backup.ID) != nil {
-		return s.r.Save(backup), nil
-	} else {
-		return nil, &services.AppError{
-			Error:   fmt.Errorf("not found: %s", "NewBackup"),
+	var err *services.AppError
+
+	if s.r.FindById(backup.ID) == nil {
+		err = &services.AppError{
+			Error:   fmt.Errorf("not found: %s", "UpdateBackup"),
 			Message: "not found",
-			Code:    int(services.NewBackupNotFound),
+			Code:    int(services.UpdateBackupNotFound),
+		}
+		return nil, err
+	}
+
+	o := s.r.Save(backup)
+
+	if o == nil {
+		err = &services.AppError{
+			Error:   fmt.Errorf("internal server error: %s", "UpdateBackup"),
+			Message: "internal server error",
+			Code:    int(services.UpdateBackupInternalServerError),
 		}
 	}
+
+	return o, err
 }
 
 func (s *BackupService) GetBackups() ([]*models.Backup, *services.AppError) {
-	return s.r.FindAll(), nil
+	var err *services.AppError
+
+	o := s.r.FindAll()
+
+	if o == nil {
+		err = &services.AppError{
+			Error:   fmt.Errorf("internal server error: %s", "GetBackups"),
+			Message: "internal server error",
+			Code:    int(services.GetBackupsInternalServerError),
+		}
+	}
+
+	return o, err
 }
 
 func (s *BackupService) GetBackupById(id int64) (*models.Backup, *services.AppError) {
-	return s.r.FindById(id), nil
+	var err *services.AppError
+
+	o := s.r.FindById(id)
+
+	if o == nil {
+		err = &services.AppError{
+			Error:   fmt.Errorf("not found: %s", "GetBackupById"),
+			Message: "not found",
+			Code:    int(services.GetBackupByIdNotFound),
+		}
+	}
+
+	return o, err
 }
 
 //
@@ -71,48 +120,97 @@ type RestorationService struct {
 }
 
 func (s *RestorationService) NewRestoration(backup *models.Backup) (*models.Restoration, *services.AppError) {
+	var err *services.AppError
+
 	b := backup // Trust client data?
 	//b := GetBackupByID((*backup).ID) // More robust method
 
 	// Backup must exists before it can be restored.
 	// If an invalid ID is provided, then restoration is skipped.
-	if b != nil {
-		log.Printf("Backup found: %d = %s\n", (*b).ID, (*b).Time)
-		r := models.Restoration{
-			ID:     -1,
-			Backup: b,
-			Status: models.RestorationStatusQueued,
+	if b == nil {
+		err = &services.AppError{
+			Error:   fmt.Errorf("backup not found: %s", "NewRestoration"),
+			Message: "backup not found",
+			Code:    int(services.NewRestorationInternalServerError),
 		}
+		return nil, err
+	}
 
-		return s.r.Save(&r), nil
-	} else {
-		log.Println("[Error] Backup not found")
-		return nil, &services.AppError{
-			Error:   fmt.Errorf("not found: %s", "NewRestoration"),
-			Message: "not found",
-			Code:    int(services.NewRestorationNotFound),
+	log.Printf("Backup found: %d = %s\n", (*b).ID, (*b).Time)
+	r := models.Restoration{
+		ID:     -1,
+		Backup: b,
+		Status: models.RestorationStatusQueued,
+	}
+
+	o := s.r.Save(&r)
+
+	if o == nil {
+		err = &services.AppError{
+			Error:   fmt.Errorf("internal server error: %s", "NewRestoration"),
+			Message: "internal server error",
+			Code:    int(services.NewRestorationInternalServerError),
 		}
 	}
+
+	return o, err
 }
 
 func (s *RestorationService) UpdateRestoration(restoration *models.Restoration) (*models.Restoration, *services.AppError) {
-	if s.r.FindById(restoration.ID) != nil {
-		return s.r.Save(restoration), nil
-	} else {
-		return nil, &services.AppError{
+	var err *services.AppError
+
+	if s.r.FindById(restoration.ID) == nil {
+		err = &services.AppError{
 			Error:   fmt.Errorf("not found: %s", "UpdateRestoration"),
 			Message: "not found",
 			Code:    int(services.UpdateRestorationNotFound),
 		}
+		return nil, err
 	}
+
+	o := s.r.Save(restoration)
+
+	if o == nil {
+		err = &services.AppError{
+			Error:   fmt.Errorf("internal server error: %s", "UpdateRestoration"),
+			Message: "internal server error",
+			Code:    int(services.UpdateRestorationInternalServerError),
+		}
+	}
+
+	return o, err
 }
 
 func (s *RestorationService) GetRestorations() ([]*models.Restoration, *services.AppError) {
-	return s.r.FindAll(), nil
+	var err *services.AppError
+
+	o := s.r.FindAll()
+
+	if o == nil {
+		err = &services.AppError{
+			Error:   fmt.Errorf("internal server error: %s", "GetRestorations"),
+			Message: "internal server error",
+			Code:    int(services.UpdateRestorationInternalServerError),
+		}
+	}
+
+	return o, err
 }
 
 func (s *RestorationService) GetRestorationById(id int64) (*models.Restoration, *services.AppError) {
-	return s.r.FindById(id), nil
+	var err *services.AppError
+
+	o := s.r.FindById(id)
+
+	if o == nil {
+		err = &services.AppError{
+			Error:   fmt.Errorf("not found: %s", "GetRestorationById"),
+			Message: "not found",
+			Code:    int(services.UpdateRestorationNotFound),
+		}
+	}
+
+	return o, err
 }
 
 //
@@ -124,48 +222,100 @@ type TransferService struct {
 }
 
 func (s *TransferService) NewTransfer(backup *models.Backup) (*models.Transfer, *services.AppError) {
+	var err *services.AppError
+
 	b := backup // Trust client data?
 	//b := GetBackupByID((*backup).ID) // More robust method
 
 	// Backup must exists before it can be restored.
 	// If an invalid ID is provided, then restoration is skipped.
-	if b != nil {
-		log.Printf("Backup found: %d = %s\n", (*b).ID, (*b).Time)
-		t := models.Transfer{
-			ID:     -1,
-			Backup: b,
-			Status: models.RestorationStatusQueued,
-		}
-
-		return s.r.Save(&t), nil
-	} else {
+	if b == nil {
 		log.Println("[Error] Backup not found")
-		return nil, &services.AppError{
+		err = &services.AppError{
 			Error:   fmt.Errorf("not found: %s", "NewTransfer"),
 			Message: "not found",
 			Code:    int(services.NewTransferNotFound),
 		}
+
+		return nil, err
 	}
+
+	log.Printf("Backup found: %d = %s\n", (*b).ID, (*b).Time)
+	t := models.Transfer{
+		ID:     -1,
+		Backup: b,
+		Status: models.RestorationStatusQueued,
+	}
+
+	o := s.r.Save(&t)
+
+	if o == nil {
+		err = &services.AppError{
+			Error:   fmt.Errorf("internal server error: %s", "NewTransfer"),
+			Message: "internal server error",
+			Code:    int(services.NewTransferInternalServerError),
+		}
+	}
+
+	return o, err
 }
 
 func (s *TransferService) UpdateTransfer(transfer *models.Transfer) (*models.Transfer, *services.AppError) {
-	if s.r.FindById(transfer.ID) != nil {
-		return s.r.Save(transfer), nil
-	} else {
-		return nil, &services.AppError{
+	var err *services.AppError
+
+	if s.r.FindById(transfer.ID) == nil {
+		err = &services.AppError{
 			Error:   fmt.Errorf("not found: %s", "UpdateTransfer"),
 			Message: "not found",
 			Code:    int(services.UpdateTransferNotFound),
 		}
+
+		return nil, err
 	}
+
+	o := s.r.Save(transfer)
+
+	if o == nil {
+		err = &services.AppError{
+			Error:   fmt.Errorf("internal server error: %s", "UpdateTransfer"),
+			Message: "internal server error",
+			Code:    int(services.UpdateTransferInternalServerError),
+		}
+	}
+
+	return o, err
 }
 
 func (s *TransferService) GetTransfers() ([]*models.Transfer, *services.AppError) {
-	return s.r.FindAll(), nil
+	var err *services.AppError
+
+	o := s.r.FindAll()
+
+	if o == nil {
+		err = &services.AppError{
+			Error:   fmt.Errorf("internal server error: %s", "GetTransfers"),
+			Message: "internal server error",
+			Code:    int(services.GetTransfersInternalServerError),
+		}
+	}
+
+	return o, err
 }
 
 func (s *TransferService) GetTransferById(id int64) (*models.Transfer, *services.AppError) {
-	return s.r.FindById(id), nil
+	var err *services.AppError
+
+	o := s.r.FindById(id)
+
+	if o == nil {
+		err = &services.AppError{
+			Error:   fmt.Errorf("not found: %s", "GetTransferById"),
+			Message: "not found",
+			Code:    int(services.GetTransferByIdNotFound),
+		}
+	}
+
+	return o, err
 }
 
 //
@@ -177,25 +327,50 @@ type SynchronizationService struct {
 }
 
 func (s *SynchronizationService) NewSynchronization() (*models.Synchronization, *services.AppError) {
+	var err *services.AppError
+
 	sync := models.Synchronization{
 		ID:     -1,
 		Status: models.LogStatusQueued,
 		Time:   strfmt.DateTime(time.Now()),
 	}
 
-	return s.r.Save(&sync), nil
+	o := s.r.Save(&sync)
+
+	if o == nil {
+		err = &services.AppError{
+			Error:   fmt.Errorf("internal server error: %s", "NewSynchronization"),
+			Message: "internal server error",
+			Code:    int(services.NewSynchronizationInternalServerError),
+		}
+	}
+
+	return o, err
 }
 
 func (s *SynchronizationService) UpdateSynchronization(synchronization *models.Synchronization) (*models.Synchronization, *services.AppError) {
-	if s.r.FindById(synchronization.ID) != nil {
-		return s.r.Save(synchronization), nil
-	} else {
-		return nil, &services.AppError{
+	var err *services.AppError
+
+	if s.r.FindById(synchronization.ID) == nil {
+		err = &services.AppError{
 			Error:   fmt.Errorf("not found: %s", "UpdateSynchronization"),
 			Message: "not found",
 			Code:    int(services.UpdateSynchronizationNotFound),
 		}
+		return nil, err
 	}
+
+	o := s.r.Save(synchronization)
+
+	if o == nil {
+		err = &services.AppError{
+			Error:   fmt.Errorf("internal server error: %s", "UpdateSynchronization"),
+			Message: "internal server error",
+			Code:    int(services.UpdateSynchronizationInternalServerError),
+		}
+	}
+
+	return o, err
 }
 
 //
@@ -207,23 +382,19 @@ type QuotaService struct {
 }
 
 func (s *QuotaService) GetQuotas() ([]*models.Quota, *services.AppError) {
-	/*
-		q1 := models.Quota{
-			ID:   -1,
-			Path: "/home/a/q1",
-			Soft: rand.Int63(),
-			Hard: rand.Int63(),
-		}
+	var err *services.AppError
 
-		q2 := models.Quota{
-			ID:   -1,
-			Path: "/home/b/q2",
-			Soft: rand.Int63(),
-			Hard: rand.Int63(),
-		}
-	*/
+	o := s.r.FindAll()
 
-	return s.r.FindAll(), nil
+	if o == nil {
+		err = &services.AppError{
+			Error:   fmt.Errorf("internal server error: %s", "GetQuotas"),
+			Message: "internal server error",
+			Code:    int(services.GetQuotasInternalServerError),
+		}
+	}
+
+	return o, err
 }
 
 //
@@ -235,6 +406,8 @@ type LogService struct {
 }
 
 func (s *LogService) NewLog() (*models.Log, *services.AppError) {
+	var err *services.AppError
+
 	l := models.Log{
 		ID:     -1,
 		Status: models.LogStatusQueued,
@@ -242,23 +415,59 @@ func (s *LogService) NewLog() (*models.Log, *services.AppError) {
 		URL:    "/v1/logs/" + strconv.FormatInt(0, 10),
 	}
 
-	return s.r.Save(&l), nil
+	o := s.r.Save(&l)
+
+	if o == nil {
+		err = &services.AppError{
+			Error:   fmt.Errorf("internal server error: %s", "NewLog"),
+			Message: "internal server error",
+			Code:    int(services.NewLogInternalServerError),
+		}
+	}
+
+	return o, err
 }
 
 func (s *LogService) UpdateLog(log *models.Log) (*models.Log, *services.AppError) {
-	if s.r.FindById(log.ID) != nil {
-		return s.r.Save(log), nil
-	} else {
-		return nil, &services.AppError{
+	var err *services.AppError
+
+	if s.r.FindById(log.ID) == nil {
+		err = &services.AppError{
 			Error:   fmt.Errorf("not found: %s", "UpdateLog"),
 			Message: "not found",
 			Code:    int(services.UpdateLogNotFound),
 		}
+
+		return nil, err
 	}
+
+	o := s.r.Save(log)
+
+	if o == nil {
+		err = &services.AppError{
+			Error:   fmt.Errorf("internal server error: %s", "UpdateLog"),
+			Message: "internal server error",
+			Code:    int(services.UpdateLogInternalServerError),
+		}
+	}
+
+	return o, err
 }
 
 func (s *LogService) GetLogById(id int64) (*models.Log, *services.AppError) {
-	return s.r.FindById(id), nil
+	var err *services.AppError
+
+	o := s.r.FindById(id)
+
+	if o == nil {
+		err = &services.AppError{
+			Error:   fmt.Errorf("not found: %s", "GetLogById"),
+			Message: "not found",
+			Code:    int(services.GetLogByIdNotFound),
+		}
+	}
+
+	return o, err
 }
 
 //
@@ -270,6 +479,8 @@ type ArchiveService struct {
 }
 
 func (s *ArchiveService) NewArchive() (*models.Archive, *services.AppError) {
+	var err *services.AppError
+
 	a := models.Archive{
 		ID:     -1,
 		Status: models.ArchiveStatusQueued,
@@ -277,25 +488,73 @@ func (s *ArchiveService) NewArchive() (*models.Archive, *services.AppError) {
 		URL:    "/v1/archives/" + strconv.FormatInt(0, 10),
 	}
 
-	return s.r.Save(&a), nil
+	o := s.r.Save(&a)
+
+	if o == nil {
+		err = &services.AppError{
+			Error:   fmt.Errorf("internal server error: %s", "NewArchive"),
+			Message: "internal server error",
+			Code:    int(services.NewArchiveInternalServerError),
+		}
+	}
+
+	return o, err
 }
 
 func (s *ArchiveService) UpdateArchive(archive *models.Archive) (*models.Archive, *services.AppError) {
-	if s.r.FindById(archive.ID) != nil {
-		return s.r.Save(archive), nil
-	} else {
-		return nil, &services.AppError{
+	var err *services.AppError
+
+	if s.r.FindById(archive.ID) == nil {
+		err = &services.AppError{
 			Error:   fmt.Errorf("not found: %s", "UpdateArchive"),
 			Message: "not found",
 			Code:    int(services.UpdateArchiveNotFound),
 		}
+
+		return nil, err
 	}
+
+	o := s.r.Save(archive)
+
+	if o == nil {
+		err = &services.AppError{
+			Error:   fmt.Errorf("internal server error: %s", "UpdateArchive"),
+			Message: "internal server error",
+			Code:    int(services.UpdateArchiveInternalServerError),
+		}
+	}
+
+	return o, err
 }
 
 func (s *ArchiveService) GetArchives() ([]*models.Archive, *services.AppError) {
-	return s.r.FindAll(), nil
+	var err *services.AppError
+
+	o := s.r.FindAll()
+
+	if o == nil {
+		err = &services.AppError{
+			Error:   fmt.Errorf("internal server error: %s", "GetArchives"),
+			Message: "internal server error",
+			Code:    int(services.GetArchivesInternalServerError),
+		}
+	}
+
+	return o, err
 }
 
 func (s *ArchiveService) GetArchiveById(id int64) (*models.Archive, *services.AppError) {
-	return s.r.FindById(id), nil
+	var err *services.AppError
+
+	o := s.r.FindById(id)
+
+	if o == nil {
+		err = &services.AppError{
+			Error:   fmt.Errorf("not found: %s", "GetArchiveById"),
+			Message: "not found",
+			Code:    int(services.GetArchiveByIdNotFound),
+		}
+	}
+
+	return o, err
 }
